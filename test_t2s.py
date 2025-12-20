fd3f04cb67ea2bc5d3c128a12135fd68b54c9a79
navjotsingh8952 <navjotsingh8952@gmail.com>
2025-12-20 17:25:18 +0530

    minor changes

--- test_t2s.py
+++ test_t2s.py
@@ -4,9 +4,14 @@ _engine = pyttsx3.init()
 _engine.setProperty("rate", 150)
 _engine.setProperty("volume", 1.0)
 
+
 def speak(text: str):
     """
     Speak text using offline TTS
     """
     _engine.say(text)
     _engine.runAndWait()
+
+
+if __name__ == '__main__':
+    speak("testing")
