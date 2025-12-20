32520e33b578cf7aebe200fd610242f0f34f3184
navjotsingh8952 <navjotsingh8952@gmail.com>
2025-12-20 17:33:10 +0530

    minor changes

--- test_t2s.py
+++ test_t2s.py
@@ -1,11 +1,9 @@
-from gtts import gTTS
 import os
-import uuid
+
 
 def speak(text):
-    file = f"/tmp/{uuid.uuid4().hex}.mp3"
-    gTTS(text=text, lang="en").save(file)
-    os.system(f"mpg123 {file}")
+    os.system(f'flite -t "{text}"')
+
 
 if __name__ == '__main__':
     speak("testing")
