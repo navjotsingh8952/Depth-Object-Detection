52328c2ae7358416647efcf6cdb58d60e3e6e21a
navjotsingh8952 <navjotsingh8952@gmail.com>
2025-12-20 17:29:55 +0530

    minor changes

--- test_t2s.py
+++ test_t2s.py
@@ -6,3 +6,6 @@ def speak(text):
     file = f"/tmp/{uuid.uuid4().hex}.mp3"
     gTTS(text=text, lang="en").save(file)
     os.system(f"mpg123 {file}")
+
+if __name__ == '__main__':
+    speak("testing")
