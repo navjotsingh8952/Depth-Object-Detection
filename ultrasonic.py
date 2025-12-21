224d74c11ef4d1d0a3627827f9f84ba750a7de36
navjotsingh8952 <navjotsingh8952@gmail.com>
2025-12-21 16:28:04 +0530

    minor changes

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,5 +1,6 @@
 from gpiozero import DistanceSensor
 
+
 class Ultrasonic:
     def __init__(self, trig, echo):
         self.sensor = DistanceSensor(
@@ -10,3 +11,11 @@ class Ultrasonic:
 
     def distance_cm(self):
         return round(self.sensor.distance * 100, 1)
+
+
+if __name__ == '__main__':
+    ultra_left = Ultrasonic(trig=23, echo=24)
+
+    while True:
+        left = ultra_left.distance_cm()
+        print(left)
